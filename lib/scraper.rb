550da0ebd4b3fcf8c89451dcf8921b5efe7f3ffd
Mohammad Khan <sparrowangels@gmail.com>
2017-11-23 07:08:30 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,84 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
+  def self.scrape_index_page(url)
+    doc = Nokogiri::HTML(open(url))
+    students = []
+    doc.css(".student-card").each do |student|
+      student_hash = {}
+      student_hash[:name] = student.css(".student-name").text
+      student_hash[:location] = student.css(".student-location").text
+      # student_hash[:profile_url] = "./fixtures/students-site/#{student.css("a").attribute("href").value}"
+      student_hash[:profile_url] = "#{student.css("a").attribute("href").value}"  #learn.co wants above, but test wants this.
+      students << student_hash
+    end #each
+    students
+  end #scrape_index_page
 
-  def self.scrape_index_page(index_url)
-    
-  end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+  #scrape_profile_page method is responsible for scraping an individual student's profile page to get further information about that student.
+#   {:twitter=>"http://twitter.com/flatironschool",
+#   :linkedin=>"https://www.linkedin.com/in/flatironschool",
+#   :github=>"https://github.com/learn-co,
+#   :blog=>"http://flatironschool.com",
+#   :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+#   :bio=> "I'm a school"
+#  }
+  def self.scrape_profile_page(url)
+    # It should use nokogiri and Open-URI to access that page.
+    # puts "***************************************#{url}"
+    doc = Nokogiri::HTML(open(url))
+    # puts doc
+    # blog =
+    twitter = ""
+    linkedin = ""
+    github = ""
+    blog = ""
+    profile_quote = doc.css("div.profile-quote").text
+    bio = doc.css("div.description-holder p").text
+    doc.css(".social-icon-container a").each do |a|
+      # puts "*******************************#{a}"
+      # puts a.css("img").attribute("src")
+      icon = a.css("img").attribute("src")
+      # if icon = "../assets/img/twitter-icon.png"
+      #   twitter=a.attribute("href").value
+      # end #if
+      # puts a.attribute("href").value # https://twitter.com/jmburges
+      # puts a.class  #Nokogiri::XML::Element
+      if a.attribute("href").value.include?("twitter")
+        twitter=a.attribute("href").value
+      elsif a.attribute("href").value.include?("linkedin")
+        linkedin=a.attribute("href").value
+      elsif a.attribute("href").value.include?("github")
+        github=a.attribute("href").value
+      else
+        blog = a.attribute("href").value
+      end #if
+
+      # twitter=a.attribute("href")
+
+    end #each
+    # puts "*********************************twitter:#{twitter}"
+    # puts "*********************************linkedin:#{linkedin}"
+    # puts "*********************************github:#{github}"
+    # puts "*********************************blog:#{blog}"
+
+    hash = {}
+    hash[:twitter] =twitter if twitter!=""
+    hash[:linkedin] =linkedin if linkedin!=""
+    hash[:github] =github if github!=""
+    hash[:blog] =blog if blog!=""
+    hash[:profile_quote] =profile_quote
+    hash[:bio] = bio
+    # puts hash
+    hash
+    # binding.pry
+    # The return value of this method should be a hash in which the key/value pairs describe an individual student.
+    #   Some students don't have a twitter or some other social link.
+  end #scrape_profile_page
 
 end
-
+# file:///home/ubuntu/dev/oo-student-scraper-cb-000/fixtures/student-site/index.html
+# Scraper.scrape_index_page("./fixtures/student-site/index.html")
--- lib/student.rb
+++ lib/student.rb
@@ -3,21 +3,55 @@ class Student
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
-
+  #*****************************************************************************************************************************************
+  # take in an argument of a hash and use metaprogramming to assign the newly created student attributes and values
+  #  in accordance with the key/value pairs of the hash
+  # Use the #send method to acheive this. This method should also add the newly created student to the Student class' @@all array of all students. 
+  # You'll need to create this class variable and set it equal to an empty array at the top of your class.
+  #  Push self into the array at the end of the #initialize method.
   def initialize(student_hash)
+    # @name=student_hash[:name]                             #works
+    # @location=  student_hash[:location]  
+    # @twitter = student_hash[:twitter] 
+    # @linkedin =  student_hash[:linkedin]  
+    # @github =  student_hash[:github]  
+    # @blog =  student_hash[:blog]  
+    # @profile_quote =  student_hash[:profile_quote]  
+    # @bio =  student_hash[:bio]  
+    # @profile_url=  student_hash[:profile_url]    
     
-  end
-
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value) #ex: converts :name,somename to name= value calling the attr_accessor method created for name
+    end #each 
+    @@all << self
+    
+  end #initialize
+  #*****************************************************************************************************************************************
+  # This class method should take in an array of hashes.
+  # iterate over the array of hashes and create a new individual student using each hash.
+  # In fact, we will call Student.create_from_collection with the return value of the Scraper.scrape_index_page method as the argument. 
   def self.create_from_collection(students_array)
-    
-  end
-
+    students_array.each do |student|
+      Student.new(student)    
+    end #each
+  end #create_from_collection
+  #*****************************************************************************************************************************************
+  # This instance method should take in a hash whose key/value pairs describe additional attributes of an individual student.
+  #    In fact, we will be calling student.add_student_attributes with the return value of the Scraper.scrape_profile_page method as the argument.
+  # The #add_student_attributes method should iterate over the given hash and use metaprogramming to dynamically assign the student attributes 
+  #   and values in accordance with the key/value pairs of the hash. Use the #send method to achieve this.
+  # Important: The return value of this method should be the student itself. Use the self keyword.  
   def add_student_attributes(attributes_hash)
-    
-  end
-
+    # puts "*******************#{attributes_hash}"
+    attributes_hash.each do |attribute, value|
+      self.send("#{attribute}=", value) #ex: converts :name,somename to name= value calling the attr_accessor method created for name
+    end #each 
+  end #add_student_attributes
+  #*****************************************************************************************************************************************
   def self.all
-    
-  end
+    @@all
+  end #all
 end
 
+# shouldn't call on the Scraper class in any of its methods or take in the Scraper class itself as an argument. 
+# Why is this? We want our program to be as flexible as possible. We
